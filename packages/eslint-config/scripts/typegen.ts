2cf973aa66778abc14a34f59088311955e3f5d63
pung <fp94214@gmail.com>
2025-08-15 14:15:37 +0800

    xx

--- packages/eslint-config/scripts/typegen.ts
+++ packages/eslint-config/scripts/typegen.ts
@@ -3,9 +3,9 @@ import fs from 'node:fs/promises'
 import { flatConfigsToRulesDTS } from 'eslint-typegen/core'
 
 import { combine } from '../src/utils'
-import { typescript } from '../src/configs/typescript'
+import { ignores, javascript } from '../src/configs/'
 
-const configs = await combine(typescript())
+const configs = await combine(ignores(), javascript())
 const configNames = configs.map(i => i.name).filter(Boolean) as string[]
 
 let dts = await flatConfigsToRulesDTS(configs, { includeAugmentation: false })
