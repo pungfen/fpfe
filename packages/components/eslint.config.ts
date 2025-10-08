08f9635114d4b798da83a5034cefacb4ec4d2791
pung <fp94214@gmail.com>
2025-10-08 23:02:07 +0800

    xx

---
+++ packages/components/eslint.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from '@fp/eslint-config'
+
+export default defineConfig({
+  tsconfigRootDir: import.meta.dirname,
+  typescript: {
+    files: ['eslint.config.ts', 'src/**/*.ts']
+  },
+  vue: true
+})
---
+++ packages/components/src/basic/XButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="emit('click')"><slot></slot></button>
+</template>
+
+<script setup lang="ts">
+export interface XButtonProps {
+  size?: 'large' | 'default' | 'small'
+  type?: 'primary' | 'success' | 'warning' | 'danger' | 'info'
+  text?: boolean
+  link?: boolean
+  loading?: boolean
+  disabled?: boolean
+  color?: string
+}
+
+const props = defineProps<XButtonProps>()
+const emit = defineEmits<{ click: [] }>()
+</script>
